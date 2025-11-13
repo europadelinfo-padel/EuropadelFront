70c75aa138da935a68fe8357faaa0d80407ae63a
europadelinfo-padel <europadelinfo@gmail.com>
2025-11-13 13:59:35 -0300

    Update page.tsx

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -129,7 +129,7 @@ export default function Login() {
               className="w-full text-black px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
             />
             <span
-              className="absolute right-3 top-2.5 cursor-pointer"
+              className="absolute right-3 top-2.5 text-black cursor-pointer"
               onClick={() => setShowPassword(!showPassword)}
             >
               {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
@@ -157,5 +157,6 @@ export default function Login() {
     </div>
   );
 }
+
 
 
