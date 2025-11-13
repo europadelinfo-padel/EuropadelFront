e0a10dd6a44017e424120bb5fc8e2dce31d553ef
europadelinfo-padel <europadelinfo@gmail.com>
2025-11-13 10:53:40 -0300

    Update page.tsx

--- app/crear-producto/page.tsx
+++ app/crear-producto/page.tsx
@@ -1361,7 +1361,7 @@ export default function VendedorProductos() {
 
         try {
          
-            const response = await fetch('http://localhost:5000/api/auth/login', {
+            const response = await fetch('https://europadel-back.vercel.app/api/auth/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
